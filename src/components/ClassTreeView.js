d4fa5d53f43264c9685d6c8a702a4beaf0c7df83
alloky <alloky2011@gmail.com>
2019-01-31 22:54:48 +0300

    async load demo

--- src/components/ClassTreeView.js
+++ src/components/ClassTreeView.js
@@ -6,35 +6,9 @@ import fetch from 'isomorphic-unfetch';
 const TreeNode = Tree.TreeNode;
 const Search = Input.Search;
 
-const x = 6;
-const y = 5;
-const z = 1;
-const gData = [];
 
-const generateData = (_level, _preKey, _tns) => {
-  const preKey = _preKey || '0';
-  const tns = _tns || gData;
-
-  const children = [];
-  for (let i = 0; i < x; i++) {
-    const key = `${preKey}-${i}`;
-    tns.push({title: key, key});
-    if (i < y) {
-      children.push(key);
-    }
-  }
-  if (_level < 0) {
-    return tns;
-  }
-  const level = _level - 1;
-  children.forEach((key, index) => {
-    tns[index].children = [];
-    return generateData(level, key, tns[index].children);
-  });
-};
-generateData(z);
-
-const testQueryReq = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+const testQueryReq = `
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX oslc: <http://open-services.net/ns/core#>
 
@@ -44,28 +18,11 @@ WHERE {
  ?x rdfs:isDefinedBy oslc:
 }`;
 
-var testQuery = `
-      PREFIX eurovoc: <http://publications.europa.eu/ontology/euvoc#>
-      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-      PREFIX eschema: <http://eurovoc.europa.eu/schema#>
 
-      SELECT ?domain ?domainLabel ?thesaurus ?thesaurusLabel
-      WHERE {
-        ?thesaurus rdf:type eschema:MicroThesaurus .
-        ?thesaurus skos:prefLabel ?thesaurusLabel .
-        ?domain rdf:type eschema:Domain .
-        ?domain skos:prefLabel ?domainLabel .
-        FILTER(langMatches(lang(?domainLabel), "###lang###"))
-        FILTER(langMatches(lang(?thesaurusLabel), "###lang###"))
-        FILTER(strStarts(?domainLabel, substr(?thesaurusLabel, 1, 2)))
-      }
-    `;
+const testServerUrlReq = "https://agentlab.ru/rdf4j-server/repositories/reqs";
 
-const testServerUrlReq = "https://agentlab.ru/rdf4j-workbench/repositories/reqs";
-const testServerUrl = "https://agentlab.ru/rdf4j-server/repositories/23";
+var dataList = [];
 
-const dataList = [];
 const generateList = (data) => {
   for (let i = 0; i < data.length; i++) {
     const node = data[i];
@@ -76,7 +33,7 @@ const generateList = (data) => {
     }
   }
 };
-generateList(gData);
+
 
 const getParentKey = (key, tree) => {
   let parentKey;
@@ -95,18 +52,55 @@ const getParentKey = (key, tree) => {
 
 class ClassTreeView extends React.Component {
   state = {
+    treeData: [
+      {title: '0', key: '0'},
+      {title: '1', key: '1'},
+      {title: '2', key: '2', isLeaf: true},
+    ],
     expandedKeys: [],
     searchValue: '',
     autoExpandParent: true,
   };
-  onExpand = async (expandedKeys) => {
-    var res = await fetch(testServerUrlReq + '?query=' + encodeURIComponent(testQueryReq), 
-      {
-        Accept: 'application/sparql-results+json'
+  onLoadData = (treeNode) => {
+    return new Promise( async (resolve) => {
+      if (treeNode.props.children) {
+        resolve();
+        return;
+      }
+      var res = await fetch(testServerUrlReq + 
+        '?query=' + 
+        encodeURIComponent(testQueryReq) + 
+        '&queryLn=sparql', 
+        { 
+          headers: {
+            Accept: 'application/sparql-results+json'
+          } 
+        });
+      console.log(testServerUrlReq + '?query=' + encodeURIComponent(testQueryReq) + '&queryLn=sparql');
+      var body = await res.json();
+      console.log(body);
+      var nodeList = [];
+      body.results.bindings.map((item) => {
+        const index = item.x.value.indexOf('#');
+        const afterStr = item.x.value.substr(index);
+        nodeList.push({
+          title: afterStr,
+          key: item.x.value
+        });
       });
-    //console.log(testServerUrl + '?query=' + encodeURIComponent(testQuery));
-    var body = await res.text();
-    console.log(body);
+      treeNode.props.dataRef.children = nodeList;
+      // treeNode.props.dataRef.children = [
+      //   {title: `${treeNode.props.eventKey}-0`, key: `${treeNode.props.eventKey}-0`, isLeaf: true},
+      //   {title: `${treeNode.props.eventKey}-1`, key: `${treeNode.props.eventKey}-1`},
+      // ];
+      this.setState({
+        treeData: [...this.state.treeData],
+        expandedKeys: [...this.state.expandedKeys]
+      });
+      resolve();
+    });
+  };
+  onExpand = (expandedKeys) => {
     this.setState({
       expandedKeys,
       autoExpandParent: false,
@@ -116,52 +110,60 @@ class ClassTreeView extends React.Component {
     const value = e.target.value;
     const expandedKeys = dataList.map((item) => {
       if (item.key.indexOf(value) > -1) {
-        return getParentKey(item.key, gData);
+        return getParentKey(item.key, this.state.treeData);
       }
       return null;
     }).filter((item, i, self) => item && self.indexOf(item) === i);
     this.setState({
       expandedKeys,
       searchValue: value,
-      autoExpandParent: false,
+      autoExpandParent: true,
+      treeData: [...this.state.treeData]
     });
   };
 
-  render() {
+  renderTreeNodes = (data) => {
     const {searchValue, expandedKeys, autoExpandParent} = this.state;
-    const loop = data => data.map((item) => {
-      const index = item.key.indexOf(searchValue);
-      const beforeStr = item.key.substr(0, index);
-      const afterStr = item.key.substr(index + searchValue.length);
+    generateList(this.state.treeData);
+    return data.map((item) => {
+      const index = item.title.indexOf(searchValue);
+      const beforeStr = item.title.substr(0, index);
+      const afterStr = item.title.substr(index + searchValue.length);
       const title = index > -1 ? (
         <span>
           {beforeStr}
           <span style={{color: '#f50'}}>{searchValue}</span>
           {afterStr}
         </span>
-      ) : <span>{item.key}</span>;
+      ) : <span>{item.title}</span>;
+
       if (item.children) {
         return (
-          <TreeNode key={item.key} title={title}>
-            {loop(item.children)}
+          <TreeNode title={title} key={item.key} dataRef={item}>
+            {this.renderTreeNodes(item.children)}
           </TreeNode>
         );
       }
-      return <TreeNode key={item.key} title={title}/>;
+      return <TreeNode title={title} key={item.key} isLeaf={item.isLeaf} dataRef={item}/>;
     });
+  };
+
+  render() {
     return (
-      <Card title="Searchable" className="gx-card">
+      <Card title="LoadData" className="gx-card">
         <Search style={{marginBottom: 8}} placeholder="Search" onChange={this.onChange}/>
-        <Tree
+        <Tree 
           onExpand={this.onExpand}
-          expandedKeys={expandedKeys}
-          autoExpandParent={autoExpandParent}
+          expandedKeys={this.state.expandedKeys}
+          autoExpandParent={this.state.autoExpandParent}
+          loadData={this.onLoadData}
         >
-          {loop(gData)}
+          {this.renderTreeNodes(this.state.treeData)}
         </Tree>
       </Card>
     );
   }
 }
+
 
 export default ClassTreeView;
