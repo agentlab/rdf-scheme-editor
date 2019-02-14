90e9f49e12edb88f6c8c5dd390cb35c0bc7f8454
alloky <alloky2011@gmail.com>
2019-02-14 21:57:32 +0300

    added file for ContextProvider

--- src/components/Controls.stories.js
+++ src/components/Controls.stories.js
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 
 import ElementsTreeAndDetailsTable from "./ElementsTreeAndDetailsTable";
 import {ElementsTreeAndDetailsTableContext} from "./ElementsTreeAndDetailsTableContext";
+import {ElementsTreeAndDetailsTableContextProvider} from "./ElementsTreeAndDetailsTableContextProvider";
 
 const elementsTreeData = [{
   title: '0-0',
@@ -98,100 +99,6 @@ function executeSparql(url, query, callback, onErr) {
       onErr(error);
       return [];
     });
-}
-
-
-export function ElementsTreeAndDetailsTableContextProvider(props) {
-  const {url, query, detailsColumns, rootElement} = props;
-
-  const [elementsTreeData, setElementsTreeData] = useState([]);
-  const [detailsData, setDetailsData] = useState([]);
-  const [expandedKeys, setExpandedKeys] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
-  const [autoExpandParent, setAutoExpandParent] = useState(true);
-
-  // можно делать интернационализацию, можно узнавать выбранную локаль и выбирать rdf лейблы по локали
-  // const { t, i18n } = useTranslation();
-
-  useEffect(
-    () => {
-      executeSparql(url, query,
-        results => {
-          //console.log("results.bindings=", results.bindings);
-          var nodeList = rootElement;
-          nodeList[0].children = results.bindings.map((item) => {
-            return({
-              title: (item.label ? item.label.value : item.type.value),
-              key: item.type.value
-            });
-          });
-          setElementsTreeData(nodeList);
-        },
-        error => {
-          console.error(error);
-        }
-      )
-    },
-    [url, query]
-  );
-
-  const onTreeSelect = (selectedKeys, info) => {
-      // console.log('onTreeSelect.info=', info);
-      // console.log('onTreeSelect.selectedKeys=', selectedKeys);
-      // console.log(rootElement);
-
-      var query = detailsQuery.replace("###", selectedKeys[0]);
-      //console.log('onTreeSelect.query=', query);
-
-      executeSparql(url, query,
-        results => {
-          //console.log("results2.bindings=", results.bindings);
-
-          var list = results.bindings.map((item, index) => {
-            return({
-              pred: item.pred.value,
-              obj: item.obj.value,
-              key: index
-            });
-          });
-          setDetailsData(list);
-        },
-        error => {
-          console.error(error);
-        }
-      )
-  }
-
-  const getExpandedKeys = (value, node, parentKey) => {
-    var keys = []
-    if (node.title.toLowerCase().indexOf(value) > -1) {
-      keys.push(parentKey);
-    }
-    node.children && node.children.forEach((child) => {
-      keys = keys.concat(getExpandedKeys(value, child, node.key));
-    });
-    return keys;
-  }
-
-  const onSearchInputChanged = (e) => {
-    const value = e.target.value.toLowerCase();
-    const expandedKeys = getExpandedKeys(value, rootElement[0], rootElement[0].key)
-      .filter((item, i, self) => item && self.indexOf(item) === i);
-    setAutoExpandParent(true);
-    setSearchValue(value);
-    setExpandedKeys(expandedKeys);
-  }
-
-  const onExpandKeys = (expandedKeys) => {
-    setAutoExpandParent(false);
-    setExpandedKeys(expandedKeys);
-  }
-
-  const value = { elementsTreeData, onTreeSelect, 
-                  detailsColumns, detailsData, 
-                  expandedKeys, onSearchInputChanged, searchValue, onExpandKeys, autoExpandParent };
-
-  return <ElementsTreeAndDetailsTableContext.Provider value={value}>{props.children}</ElementsTreeAndDetailsTableContext.Provider>
 }
 
 
