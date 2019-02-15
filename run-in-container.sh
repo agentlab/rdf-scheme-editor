1e4b2f83c0460bab43f5c3078a30f5f94e6fe92b
Alex Ivanov <amivanoff@gmail.com>
2019-02-15 20:43:49 +0300

    Обновлена версия node

--- run-in-container.sh
+++ run-in-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 3043:3000/tcp -p 3083:9009/tcp -it --rm --name rdf-scheme-editor -v "$PWD":/usr/src/app -w /usr/src/app node:8 bash
+docker run -p 3043:3000/tcp -p 3083:9009/tcp -it --rm --name rdf-scheme-editor -v "$PWD":/usr/src/app -w /usr/src/app node:11 bash
