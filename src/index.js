2a657cfd1bd92fb45885410bc4067e00e7d3e6f7
Senglyaev Dolgan <44002829+ciganeshima@users.noreply.github.com>
2019-03-10 21:53:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import rootReducer from './reducers'
 
+import {ConnectedRouter} from "react-router-redux";
+import {Route, Switch} from "react-router-dom";
 
+import "assets/vendors/style";
+import "styles/flexile.less";
 
- render(
-    <App />,
+const store = createStore(rootReducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
