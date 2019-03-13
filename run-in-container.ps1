f5736644f0edeaf74ae0a55a0418aa3de9ec864a
Alex Ivanov <amivanoff@gmail.com>
2019-03-13 12:38:36 +0300

    Добавлен пример получения списка RDF репозиториев с сервера. настроено Автообновление Storybook в docker-контейрене под windows

--- run-in-container.ps1
+++ run-in-container.ps1
@@ -1,1 +1,1 @@
-docker run -p 3043:3000/tcp -p 3083:9009/tcp -it --rm --name rdf-scheme-editor -v ${PWD}:/usr/src/app -w /usr/src/app node:11 bash
+docker run -p 3043:3000/tcp -p 3083:9009/tcp -p 35729:35729/tcp --env CHOKIDAR_USEPOLLING=true -it --rm --name rdf-scheme-editor -v ${PWD}:/usr/src/app:rw -w /usr/src/app node:11 bash
