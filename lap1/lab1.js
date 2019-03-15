5926cb80434e4d26a8ff0364289d592900e338bc
Trang5396 <cocongannho0503@gmail.com>
2019-03-15 15:40:42 +0300

    complite homework #7

---
+++ lap1/lab1.js
@@ -0,0 +1,11 @@
+import { Button } from 'antd';
+
+ReactDOM.render(
+  <div>
+    <Button type="primary">Primary</Button>
+    <Button>Default</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="danger">Danger</Button>
+  </div>,
+  mountNode
+);
