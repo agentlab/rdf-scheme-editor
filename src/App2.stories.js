b258b6431735d78023330ae06d3a6979b1a5fe3c
tatiana <tatiyana_sh@mail.ru>
2019-03-27 15:26:31 +0300

    исправила форматирование

--- src/App2.stories.js
+++ src/App2.stories.js
@@ -1,9 +1,8 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react'
-//import {Col, Row} from "antd";
-/////
+
 import {
-	Layout, Menu, Breadcrumb,Button,Dropdown, Icon,message,Pagination, AutoComplete,Checkbox, Input, Table, Divider, Tag
+	Layout, Menu, Breadcrumb, Button, Dropdown, Icon, message, Pagination, AutoComplete, Checkbox, Input, Table, Divider, Tag
 } from 'antd';
 
 import 'antd/dist/antd.css';
@@ -14,152 +13,109 @@ const SubMenu = Menu.SubMenu;
 
 const menu2 = (
 	<Menu>
-	  <Menu.Item key="1"><Icon type="user" />All</Menu.Item>
-	  <Menu.Item key="2"><Icon type="user" />10</Menu.Item>
-	  <Menu.Item key="3"><Icon type="user" />50</Menu.Item>
-	  <Menu.Item key="3"><Icon type="user" />100</Menu.Item>
-	  <Menu.Item key="3"><Icon type="user" />200</Menu.Item>	 
-	</Menu>		
-	);
+		<Menu.Item key="1"><Icon type="user" />All</Menu.Item>
+		<Menu.Item key="2"><Icon type="user" />10</Menu.Item>
+		<Menu.Item key="3"><Icon type="user" />50</Menu.Item>
+		<Menu.Item key="3"><Icon type="user" />100</Menu.Item>
+		<Menu.Item key="3"><Icon type="user" />200</Menu.Item>
+	</Menu>
+);
 
-	const columns = [{
-		title: 'Subject',
-		dataIndex: 'name',
-		key: 'name',
-		render: text => <a href="javascript:;">{text}</a>,
-	}, {
-		title: 'Predicate',
-		dataIndex: 'age',
-		key: 'age',
-	}, {
-		title: 'Object',
-		dataIndex: 'address',
-		key: 'address',
-	}, {
-		title: 'Context',
-		key: 'tags',
-		dataIndex: 'tags',
-		render: tags => (
-			<span>
-				{tags.map(tag => {
-					let color = tag.length > 5 ? 'geekblue' : 'green';
-					if (tag === 'loser') {
-						color = 'volcano';
-					}
-					return <Tag color={color} key={tag}>{tag.toUpperCase()}</Tag>;
-				})}
-			</span>
-		),
+const columns = [{
+	title: 'Subject',
+	dataIndex: 'name',
+	key: 'name',
+	render: text => <a href="javascript:;">{text}</a>,
+}, {
+	title: 'Predicate',
+	dataIndex: 'age',
+	key: 'age',
+}, {
+	title: 'Object',
+	dataIndex: 'address',
+	key: 'address',
+}, {
+	title: 'Context',
+	key: 'tags',
+	dataIndex: 'tags',
+	render: tags => (
+		<span>
+			{tags.map(tag => {
+				let color = tag.length > 5 ? 'geekblue' : 'green';
+				if (tag === 'loser') {
+					color = 'volcano';
+				}
+				return <Tag color={color} key={tag}>{tag.toUpperCase()}</Tag>;
+			})}
+		</span>
+	),
 
-	}];
-
-	
-	const data = [{
-		key: '1',
-		name: '',
-		address: '',
-		tags: [],
-	}, {
-		key: '2',
-		name: '',
-	
-		address: '',
-		tags: [],
-	}, {
-		key: '',
-		name: '',
-		
-		address: '',
-		tags: [],
-	}];
+}];
 
 
+const data = [{
+	key: '1',
+	name: '',
+	address: '',
+	tags: [],
+}, {
+	key: '2',
+	name: '',
 
-    class Explore extends React.Component {
-        render() {
-	return (
-		<Layout>
-			
-            
-			<Layout >
-           
+	address: '',
+	tags: [],
+}, {
+	key: '',
+	name: '',
 
-
-				
-			
+	address: '',
+	tags: [],
+}];
 
 
 
-				<Layout style={{ marginLeft: 200, padding: '0 24px 24px' }}>
-					<Breadcrumb style={{ margin: '16px 0' }}>
-						<Breadcrumb.Item>Explore</Breadcrumb.Item>
-	
-					</Breadcrumb>
+class Explore extends React.Component {
+	render() {
+		return (
+			<Layout>
 
 
-					<Checkbox>Show data types & language tags</Checkbox>
+				<Layout >
 
-   
+					<Layout style={{ marginLeft: 200, padding: '0 24px 24px' }}>
+						<Breadcrumb style={{ margin: '16px 0' }}>
+							<Breadcrumb.Item>Explore</Breadcrumb.Item>
+						</Breadcrumb>
 
+						<Checkbox>Show data types & language tags</Checkbox>
 
+						<Content style={{
+							margin: '100px 100px 100px', overflow: 'initial'
+						}}>
 
+							<Input placeholder="Resource" style={{ marginLeft: 7, right: 100, top: -100, bottom: 0 }} />
 
-					<Content style={{
-						margin: '100px 100px 100px', overflow: 'initial'
-					}}>
+							<Dropdown overlay={menu2}>
+								<Button style={{ marginLeft: 8, right: 100, top: -80, bottom: 0 }}>100 <Icon type="down" /></Button>
+							</Dropdown>
 
-		
+							<div >
+								<Button type="primary" style={{ marginLeft: 8, right: 100, top: -60, bottom: 0 }}>Previous 100</Button>
+								<Button type="primary" style={{ marginLeft: 8, right: 100, top: -60, bottom: 0 }}>Next 100</Button>
+							</div>
 
+							<Table columns={columns} dataSource={data} />
 
-		
-        <Input placeholder="Resource" style={{ marginLeft: 7,  right:100, top:-100, bottom:0 }}/>
-
-
-
-
-        <Dropdown overlay={menu2}>
-				<Button style={{ marginLeft: 8,  right:100, top:-80, bottom:0 }}>100 <Icon type="down" />
-				
-							 </Button>
-							 
-         </Dropdown>
-
-
-
-
-				 
-
-				 
-
-
-				 <div >
-<Button type="primary" style={{ marginLeft: 8,  right:100, top:-60, bottom:0 }}>Previous 100</Button>
-<Button type="primary" style={{marginLeft: 8,  right:100, top:-60, bottom:0 }}>Next 100</Button>
-
-	</div>
-
-
-
-
-<Table columns={columns} dataSource={data}   />
-
-
-
-
-
-					</Content>
-					
-					
+						</Content>
+					</Layout>
 				</Layout>
 			</Layout>
-		</Layout>
-        
-    );
-    
+
+		);
+	}
 }
-    }
 
 storiesOf('Issue #5', module)
-  .add('Explore', () => (
-    <Explore />
-  ))
+	.add('Explore', () => (
+		<Explore />
+	))
