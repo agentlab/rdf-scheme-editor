3a2fff92268b7ded26964559fa9e0c1ede70673d
Alex Ivanov <amivanoff@gmail.com>
2019-04-17 11:00:16 +0300

    Test refactored

--- src/rdf4j-tests/repositories.test.js
+++ src/rdf4j-tests/repositories.test.js
@@ -1,12 +1,23 @@
 const url = 'https://agentlab.ru/rdf4j-server/repositories';
 
 test('the data is peanut butter', async () => {
-    expect.assertions(1);
-    const data = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/sparql-results+json'
-        }
-    });
-    expect(data).toBe('peanut butter');
+  expect.assertions(1);
+  const data = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/sparql-results+json',
+    },
+  }).then((r) => r.json());
+
+  //console.log(data);
+  const repositories = data.results.bindings.map((binding) => ({
+    key: binding.id.value,
+    id: binding.id.value,
+    title: binding.title.value,
+    uri: binding.uri.value,
+    readable: binding.readable.value,
+    writable: binding.writable.value,
+  }));
+
+  expect(repositories.length).toBeGreaterThan(0);
 });
