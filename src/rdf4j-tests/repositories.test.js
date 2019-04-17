17040eec4a9b234d180d52cf65c7a6cdf731017a
Defasium <defasium@yandex.ru>
2019-04-17 11:41:13 +0300

    ss

--- src/rdf4j-tests/repositories.test.js
+++ src/rdf4j-tests/repositories.test.js
@@ -1,42 +1,12 @@
 const url = 'https://agentlab.ru/rdf4j-server/repositories';
 
 test('the data is peanut butter', async () => {
-    expect.assertions(1);
-    const data = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/sparql-results+json'
-        }
-    });
-    expect(data).toBe('peanut butter');
-});
-
-test('insert data to rdf repository without context', async () => {
-    const url = '/rdf4j-server/repositories/1488/transactions/1?action=UPDATE'
-    expect.assertions(1);
-    const data = await fetch(url, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            data: 'kek'
-        },
-    });
-    expect(data).toBe('peanut butter');
-});
-
-test('insert data to rdf repository without context', async () => {
-    const url = '/rdf4j-server/repositories/1488/transactions/1?action=UPDATE'
-    expect.assertions(1);
-    const data = await fetch(url, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: {
-            data: 'kek'
-        },
-    });
-    expect(data).toBe('peanut butter');
+  expect.assertions(1);
+  const data = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/sparql-results+json',
+    },
+  });
+  expect(data).toBe('peanut butter');
 });
