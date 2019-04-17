341275dd2b56dfb484abe5f755463fa7fed19fd7
Enfaseel <narek-olkinyan@hotmail.com>
2019-04-17 11:40:35 +0300

    добавление и удаление репошек

--- src/rdf4j-tests/repositories.test.js
+++ src/rdf4j-tests/repositories.test.js
@@ -1,12 +1,34 @@
-const url = 'https://agentlab.ru/rdf4j-server/repositories';
+test('add repository mem', async () => {
+  const url = 'https://agentlab.ru/rdf4j-workbench/repositories/mem-rdf/create';
+  const data = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data: {
+      data:
+        'type=memory&Repository+ID=mem&Repository+title=&Persist=true&Sync+delay=0&EvaluationStrategyFactory=org.eclipse.rdf4j.query.algebra.evaluation.impl.StrictEvaluationStrategyFactory',
+    },
+  }).then((r) => {
+    console.log(r);
+    return r;
+  });
+  expect(data).toBe('HTTP/1.1 204 NO CONTENT');
+});
 
-test('the data is peanut butter', async () => {
-    expect.assertions(1);
-    const data = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/sparql-results+json'
-        }
-    });
-    expect(data).toBe('peanut butter');
+test('remove repository mem', async () => {
+  const url = 'Request URL: https://agentlab.ru/rdf4j-workbench/repositories/NONE/delete';
+  const data = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    data: {
+      data: 'id=mem',
+    },
+  }).then((r) => {
+    console.log(r);
+    return r;
+  });
+  expect(data).toBe('HTTP/1.1 204 NO CONTENT');
 });
