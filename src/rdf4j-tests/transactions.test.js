af98f3ecf27c8287b02108b766be8f8cad3a25aa
johndahrah <dahrah@yandex.ru>
2019-05-08 13:08:21 +0300

    Server returns 403 error on PUT request

--- src/rdf4j-tests/transactions.test.js
+++ src/rdf4j-tests/transactions.test.js
@@ -1,12 +1,21 @@
 const url = 'https://agentlab.ru/rdf4j-server/repositories/rpo-tests/transactions';
 
 test('test transaction', async () => {
-  //expect.assertions(1);
   const transactionData = await fetch(url, {
     method: 'POST',
   });
-  console.log(transactionData);
-  let locationURL = transactionData.headers.get('Location'); // null
-  console.warn(locationURL);
+  let locationURL = transactionData.headers.get('Location');
   expect(locationURL).not.toBeNull();
+
+  var transactionURL = locationURL.concat('?action=QUERY');
+  console.warn(transactionURL);
+
+  try {
+    const queryResult = await fetch(transactionURL, {
+      method: 'PUT',
+      header: 'Accept: text/turtle',
+    });
+  } catch (error) {
+    console.error(error); // 403 HTTP
+  }
 });
