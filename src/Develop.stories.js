20b3ab0f2d59715d0ff76e1b679d064d4e07f156
const-er <eremi4ev.k@yandex.ru>
2019-05-15 11:36:11 +0300

    Develop in Unified view

--- src/Develop.stories.js
+++ src/Develop.stories.js
@@ -1,7 +1,38 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Table, Input, Select, Checkbox, Button, Divider } from 'antd';
+import { Table, Input, Select, Checkbox, Button, Divider, Tree } from 'antd';
 //import { Z_BLOCK } from 'zlib';
+const { TreeNode } = Tree;
+
+const treeData = [
+  {
+    title: 'Extractors',
+    key: '1',
+    children: [
+      {
+        title: 'Extractor1',
+        key: '2',
+      },
+      {
+        title: 'Extractor2',
+        key: '3',
+      },
+      {
+        title: 'Extractor3',
+        key: '4',
+      },
+    ],
+  },
+  {
+    title: 'Transformers',
+    key: '5',
+    children: [
+      { title: 'Transformer1', key: '6' },
+      { title: 'Transformer2', key: '7' },
+      { title: 'Transformer3', key: '8' },
+    ],
+  },
+];
 
 //const Option = Select.Option;
 //const { TextArea } = Input;
@@ -14,9 +45,17 @@ const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 class Develop extends React.Component {
-  handleClick = (e) => {
-    console.log('click ', e);
-  };
+  renderTreeNodes = (data) =>
+    data.map((item) => {
+      if (item.children) {
+        return (
+          <TreeNode title={item.title} key={item.key} dataRef={item}>
+            {this.renderTreeNodes(item.children)}
+          </TreeNode>
+        );
+      }
+      return <TreeNode {...item} />;
+    });
 
   render() {
     return (
@@ -32,75 +71,7 @@ class Develop extends React.Component {
           <Input placeholder='Type to filter tree' />
           <div style={{ height: 10 }} />
         </div>
-        <Menu
-          onClick={this.handleClick}
-          style={{ width: 256 }}
-          defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
-          mode='inline'>
-          <SubMenu
-            key='sub1'
-            title={
-              <span>
-                <Icon type='folder' />
-                <span>Extractors</span>
-              </span>
-            }>
-            <MenuItemGroup key='g1'>
-              <SubMenu key='sub2' title='Extractor 1'>
-                <Menu.Item key='2'>Option</Menu.Item>
-              </SubMenu>
-              <SubMenu key='sub3' title='Extractor 2'>
-                <Menu.Item key='3'>Option</Menu.Item>
-              </SubMenu>
-              <SubMenu key='sub4' title='Extractor 3'>
-                <Menu.Item key='4'>Option</Menu.Item>
-              </SubMenu>
-              <SubMenu key='sub5' title='Extractor 4'>
-                <Menu.Item key='5'>Option</Menu.Item>
-              </SubMenu>
-              <SubMenu key='sub6' title='Extractor 5'>
-                <Menu.Item key='6'>Option</Menu.Item>
-              </SubMenu>
-              <SubMenu key='sub7' title='Extractor 6'>
-                <Menu.Item key='7'>Option</Menu.Item>
-              </SubMenu>
-              <SubMenu key='sub8' title='Extractor 7'>
-                <Menu.Item key='8'>Option</Menu.Item>
-              </SubMenu>
-            </MenuItemGroup>
-          </SubMenu>
-          <SubMenu
-            key='sub9'
-            title={
-              <span>
-                <Icon type='folder' />
-                <span>Transformers</span>
-              </span>
-            }>
-            <SubMenu key='sub10' title='Transformer 1'>
-              <Menu.Item key='9'>Option</Menu.Item>
-            </SubMenu>
-            <SubMenu key='sub11' title='Transformer 2'>
-              <Menu.Item key='10'>Option</Menu.Item>
-            </SubMenu>
-            <SubMenu key='sub12' title='Transformer 3'>
-              <Menu.Item key='11'>Option</Menu.Item>
-            </SubMenu>
-            <SubMenu key='sub13' title='Transformer 4'>
-              <Menu.Item key='12'>Option</Menu.Item>
-            </SubMenu>
-            <SubMenu key='sub14' title='Transformer 5'>
-              <Menu.Item key='13'>Option</Menu.Item>
-            </SubMenu>
-            <SubMenu key='sub15' title='Transformer 6'>
-              <Menu.Item key='14'>Option</Menu.Item>
-            </SubMenu>
-            <SubMenu key='sub16' title='Transformer 7'>
-              <Menu.Item key='15'>Option</Menu.Item>
-            </SubMenu>
-          </SubMenu>
-        </Menu>
+        <Tree>{this.renderTreeNodes(treeData)}</Tree>
       </div>
     );
   }
