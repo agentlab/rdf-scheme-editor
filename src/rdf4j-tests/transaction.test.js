351e0b8f91eb63183e76aaf88ad934c3e7fa0123
tatiana <tatiyana_sh@mail.ru>
2019-05-18 22:38:59 +0300

    test

---
+++ src/rdf4j-tests/transaction.test.js
@@ -0,0 +1,21 @@
+const url = 'https://agentlab.ru/rdf4j-server/repositories/rpo-tests/transactions';
+
+test('test transaction', async () => {
+  const transactionData = await fetch(url, {
+    method: 'POST',
+  });
+  let locationURL = transactionData.headers.get('Location');
+  expect(locationURL).not.toBeNull();
+
+  var transactionURL = locationURL.concat('?action=QUERY');
+  console.warn(transactionURL);
+
+  try {
+    const queryResult = await fetch(transactionURL, {
+      method: 'PUT',
+      header: 'Accept: text/turtle',
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
