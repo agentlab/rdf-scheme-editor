98b884eb9897af77a0911d3c7b9cf37b2b606096
Defasium <defasium@yandex.ru>
2019-05-26 21:10:41 +0300

    Тестирование INSERT и DELETE SPARQL с GET без контекста #53

--- src/rdf4j-tests/repositories.test.js
+++ src/rdf4j-tests/repositories.test.js
@@ -1,44 +1,33 @@
-const url = 'https://agentlab.ru/rdf4j-server/repositories';
-
-test('the data is peanut butter', async () => {
-  expect.assertions(1);
-  const data = await fetch(url, {
+test('insert data - statement - to rdf repository without context', () => {
+  const urlprefix = 'https://agentlab.ru/rdf4j-server/repositories/qqqqqq/statements';
+  const query =
+    '?update=PREFIX%20dc%3A%20%3Chttp%3A%2F%2Fld-r.org%2Fconfig%2Ffacetproperty_creator_adms3%3E%20INSERT%20DATA%7B%20GRAPH%20%3Chttp%3A%2F%2Fexample%2Fbooks%3E%20%7B%3Chttp%3A%2F%2Fexample%2Fbook0%2F%3E%20dc%3Atitle%20%22C%20book%22%7D%20%7D';
+  // console.log(urlprefix + query);
+  return fetch(urlprefix + query, {
     method: 'GET',
     headers: {
-      Accept: 'application/sparql-results+json',
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-  }).then((r) => r.json());
-
-  //console.log(data);
-  const repositories = data.results.bindings.map((binding) => ({
-    key: binding.id.value,
-    id: binding.id.value,
-    title: binding.title.value,
-    uri: binding.uri.value,
-    readable: binding.readable.value,
-    writable: binding.writable.value,
-  }));
-
-  expect(repositories.length).toBeGreaterThan(0);
-});
-
-test('insert data to rdf repository without context', () => {
-  const url = 'http://agentlab.ru/rdf4j-server/repositories/1488/transactions';
-  //   expect.assertions(1);
-  return fetch(url, {
-    method: 'POST',
-    // headers: {
-    //   'Content-Type': 'application/x-www-form-urlencoded',
-    // },
-    // data: {
-    //   data:
-    //     'INSERT%20%7B%3FS%20a%20%3Curn%3ASinger%3E%20%20%7D%20WHERE%20%7B%20%3FS%20%3Curn%3Aname%3E%20%22John%22%20%7D',
-    // },
   }).then((r) => {
-    console.log(r);
     expect(r).toBeInstanceOf(Response);
     expect(r['status']).toEqual(200);
-    expect(r).toEqual('Response');
+    return r;
+  });
+});
+
+test('delete data - statement - to rdf repository without context', () => {
+  const urlprefix = 'https://agentlab.ru/rdf4j-server/repositories/qqqqqq/statements';
+  const query =
+    '?update=PREFIX%20dc%3A%20%3Chttp%3A%2F%2Fld-r.org%2Fconfig%2Ffacetproperty_creator_adms3%3E%20DELETE%20DATA%7B%20GRAPH%20%3Chttp%3A%2F%2Fexample%2Fbooks%3E%20%7B%3Chttp%3A%2F%2Fexample%2Fbook0%2F%3E%20dc%3Atitle%20%22C%20book%22%7D%20%7D';
+  // console.log(urlprefix + query);
+  return fetch(urlprefix + query, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  }).then((r) => {
+    expect(r).toBeInstanceOf(Response);
+    expect(r['status']).toEqual(200);
     return r;
   });
 });
