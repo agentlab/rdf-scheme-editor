094c190c0fcdb34d7c999827ed9741b91cb08fb2
BlackOwl99 <olga_protsenko_99@mail.ru>
2019-05-28 14:24:30 +0300

    https://github.com/agentlab/rdf-scheme-editor/issues/57

--- src/rdf4j-tests/repositories.test.js
+++ src/rdf4j-tests/repositories.test.js
@@ -1,23 +1,39 @@
-const url = 'https://agentlab.ru/rdf4j-server/repositories';
-
-test('the data is peanut butter', async () => {
-  expect.assertions(1);
-  const data = await fetch(url, {
+test('insert data to rdf repository with context', async () => {
+  const urlprefix = 'https://agentlab.ru/rdf4j-server/repositories/rtrtrtr/statements';
+  const data = await fetch(urlprefix, {
     method: 'GET',
     headers: {
-      Accept: 'application/sparql-results+json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Accept: 'application/rdf+xml',
     },
-  }).then((r) => r.json());
+    query:
+      'update=&#39;PREFIX dc: &lt;http://purl.org/dc/elements/1.1/&gt;\
+      INSERT DATA{ GRAPH & lt; C1 & gt; {& lt; http://example/book1/&gt; dc:title &quot;C sword&quot;} }&#39; \
+      &#39; http://localhost:8000/v1/graphs/sparql&#39;',
+  }).then((r) => {
+    console.log(r);
+    expect(r).toBeInstanceOf(Response);
+    expect(r['status']).toEqual(200);
+    return r;
+  });
+});
 
-  //console.log(data);
-  const repositories = data.results.bindings.map((binding) => ({
-    key: binding.id.value,
-    id: binding.id.value,
-    title: binding.title.value,
-    uri: binding.uri.value,
-    readable: binding.readable.value,
-    writable: binding.writable.value,
-  }));
-
-  expect(repositories.length).toBeGreaterThan(0);
+test('delete data to rdf repository with context', async () => {
+  const urlprefix = 'https://agentlab.ru/rdf4j-server/repositories/rtrtrtr/statements';
+  const data = await fetch(urlprefix, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Accept: 'application/rdf+xml',
+    },
+    query:
+      'update=&#39;PREFIX dc: &lt;http://purl.org/dc/elements/1.1/&gt;\
+      DELETE DATA{ GRAPH & lt; C1 & gt; {& lt; http://example/book1/&gt; dc:title &quot;C sword&quot;} }&#39; \
+      &#39; http://localhost:8000/v1/graphs/sparql&#39;',
+  }).then((r) => {
+    console.log(r);
+    expect(r).toBeInstanceOf(Response);
+    expect(r['status']).toEqual(200);
+    return r;
+  });
 });
