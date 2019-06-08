a4799618c656c8fbe2051d432f3c4bf7d392c923
johndahrah <dahrah@yandex.ru>
2019-06-08 13:31:24 +0300

    Request

--- src/rdf4j-tests/SparqlQueryPUT.test.js
+++ src/rdf4j-tests/SparqlQueryPUT.test.js
@@ -1,22 +1,19 @@
 const url = 'https://agentlab.ru/rdf4j-server/repositories/rpo-tests/transactions';
 
 test('Sparql query test with PUT request', async () => {
-    const tID = await fetch(url, {
-        method: 'POST',
-    }).then((r) => r.headers.get('Location'));
+  const tID = await fetch(url, {
+    method: 'POST',
+  }).then((r) => r.headers.get('Location'));
 
-    expect(tID).not.toBe(null);
-    const trURL = tID.concat('?action=GET')
+  expect(tID).not.toBe(null);
+  const trURL = tID.concat('?action=GET');
 
-    const result = await fetch(trURL, {
-        method: 'POST',
-        accept: '*/*',
-        Connection: 'keep-alive',
-    }).then((r) => {
-        expect(r.status).toBe(200);
-        expect(r.headers.get('content-type')).toBe('application/n-triples;charset=UTF-8')
-        console.log(r);
-    });
-
-
+  const result = await fetch(trURL, {
+    method: 'POST',
+    accept: '*/*',
+    Connection: 'keep-alive',
+  }).then((r) => {
+    expect(r.status).toBe(200);
+    expect(r.headers.get('content-type')).toBe('application/n-triples;charset=UTF-8');
+  });
 });
