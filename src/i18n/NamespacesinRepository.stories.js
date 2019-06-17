59479aa1390724a4cf181c719647c4cc124e2069
mr7falcon <mr.falcon.inc@gmail.com>
2019-06-17 16:50:10 +0300

    1

--- src/i18n/NamespacesinRepository.stories.js
+++ src/i18n/NamespacesinRepository.stories.js
@@ -1,107 +1,106 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Table, message } from 'antd';
-import { Form, Input, Select, Button } from 'antd';
+import { Form, Input, Select, Button, Popconfirm } from 'antd';
 import { async } from 'q';
 
-const dataSource = [
-  {
-    key: '1',
-    prefix: 'Mike',
-    namespace: 32,
-  },
-  {
-    key: '2',
-    prefix: 'John',
-    namespace: 42,
-  },
-];
+const EditableContext = React.createContext();
 
 var data = [];
 
-const columns = [
-  {
-    title: 'Prefix',
-    dataIndex: 'prefix',
-    key: 'prefix',
-  },
-  {
-    title: 'Namespace',
-    dataIndex: 'namespace',
-    key: 'namespace',
-  },
-];
+const EditableRow = ({ form, index, ...props }) => (
+  <EditableContext.Provider value={form}>
+    <tr {...props} />
+  </EditableContext.Provider>
+);
 
-const { Option } = Select;
+const EditableFormRow = Form.create()(EditableRow);
 
-class PrefixInput extends React.Component {
-  static getDerivedStateFromProps(nextProps) {
-    // Should be a controlled component.
-    if ('value' in nextProps) {
-      return {
-        ...(nextProps.value || {}),
-      };
-    }
-    return null;
-  }
-
-  handleNumberChange = (e) => {
-    const number = parseInt(e.target.value, 10);
-    if (Number.isNaN(number)) {
-      return;
-    }
-    if (!('value' in this.props)) {
-      this.setState({ number });
-    }
-    this.triggerChange({ number });
+class EditableCell extends React.Component {
+  state = {
+    editing: false,
   };
 
-  triggerChange = (changedValue) => {
-    // Should provide an event to pass value to Form.
-    const onChange = this.props.onChange;
-    if (onChange) {
-      onChange(Object.assign({}, this.state, changedValue));
-    }
-  };
-
-  render() {
-    const { size } = this.props;
-    const state = this.state;
-    return (
-      <span>
-        <Input
-          type='text'
-          size={size}
-          value={state.number}
-          onChange={this.handleNumberChange}
-          style={{ width: '65%', marginRight: '3%' }}
-        />
-        <Select value={state.currency} size={size} style={{ width: '32%' }} onChange={this.handleCurrencyChange}>
-          {data.map((option, i) => (
-            <Option value={option.namespace}>{option.prefix}</Option>
-          ))}
-        </Select>
-      </span>
-    );
-  }
-}
-
-class Demo extends React.Component {
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
+  toggleEdit = () => {
+    const editing = !this.state.editing;
+    this.setState({ editing }, () => {
+      if (editing) {
+        this.input.focus();
       }
     });
   };
 
-  checkPrefix = (rule, value, callback) => {
-    if (value.number > 0) {
-      callback();
-      return;
-    }
+  save = (e) => {
+    const { record, handleSave } = this.props;
+    this.form.validateFields((error, values) => {
+      if (error && error[e.currentTarget.id]) {
+        return;
+      }
+      this.toggleEdit();
+      handleSave({ ...record, ...values });
+    });
   };
+
+  renderCell = (form) => {
+    this.form = form;
+    const { children, dataIndex, record, title } = this.props;
+    const { editing } = this.state;
+    return editing ? (
+      <Form.Item style={{ margin: 0 }}>
+        {form.getFieldDecorator(dataIndex, {
+          rules: [
+            {
+              required: true,
+              message: `${title} is required.`,
+            },
+          ],
+          initialValue: record[dataIndex],
+        })(<Input ref={(node) => (this.input = node)} onPressEnter={this.save} onBlur={this.save} />)}
+      </Form.Item>
+    ) : (
+      <div className='editable-cell-value-wrap' style={{ paddingRight: 24 }} onClick={this.toggleEdit}>
+        {children}
+      </div>
+    );
+  };
+
+  render() {
+    const { editable, dataIndex, title, record, index, handleSave, children, ...restProps } = this.props;
+    return (
+      <td {...restProps}>
+        {editable ? <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer> : children}
+      </td>
+    );
+  }
+}
+
+class EditableTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.execut();
+    this.columns = [
+      {
+        title: 'prefix',
+        dataIndex: 'prefix',
+        width: '30%',
+        editable: true,
+      },
+      {
+        title: 'Namespace',
+        dataIndex: 'Namespace',
+      },
+      {
+        title: 'operation',
+        dataIndex: 'operation',
+        render: (text, record) =>
+          data.length >= 1 ? (
+            <Popconfirm title='Sure to delete?' onConfirm={() => this.handleDelete(record.key)}>
+              <a href='javascript:;'>Delete</a>
+            </Popconfirm>
+          ) : null,
+      },
+    ];
+  }
 
   async execut() {
     var dat = [];
@@ -121,57 +120,72 @@ class Demo extends React.Component {
       dat.push(binding);
       data = dat;
     });
-
-    /*const repositories = res.results.bindings.map((binding) => ({
-      key: binding.id.value,
-    }));
-
-    repositories.forEach(async (element) => {
-      const qu = await fetch(url + '/' + element.key + '/namespaces', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/sparql-results+json',
-        },
-      }).then((r) => r.json());
-
-      console.log(qu);
-
-      qu.results.bindings.forEach((quer) => {
-        const binding = {
-          prefix: quer.prefix.value,
-          namespace: quer.namespace.value,
-        };
-        dat.push(binding);
-      }); */
-    console.log(data);
   }
 
+  handleDelete = (key) => {
+    const dataSource = [...data];
+    data = dataSource.filter((item) => item.key !== key);
+  };
+
+  handleAdd = () => {
+    const newData = {
+      key: data.length,
+      prefix: `Edward King ${data.length}`,
+      Namespace: 32,
+    };
+    data.push(newData);
+    console.log(data);
+    this.render();
+  };
+
+  handleSave = (row) => {
+    const newData = [...this.state.dataSource];
+    const index = newData.findIndex((item) => row.key === item.key);
+    const item = newData[index];
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    });
+    this.setState({ dataSource: newData });
+  };
+
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const components = {
+      body: {
+        row: EditableFormRow,
+        cell: EditableCell,
+      },
+    };
+    const columns = this.columns.map((col) => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: (record) => ({
+          record,
+          editable: col.editable,
+          dataIndex: col.dataIndex,
+          title: col.title,
+          handleSave: this.handleSave,
+        }),
+      };
+    });
     return (
-      <Form layout='inline' onSubmit={this.handleSubmit}>
-        <Form.Item label='Prefix'>
-          {getFieldDecorator('prefix', {
-            initialValue: { currency: 'Mike' },
-            rules: [{ validator: this.checkPrefix }],
-          })(<PrefixInput />)}
-        </Form.Item>
-        <Form.Item>
-          <Button type='primary' htmlType='submit' onClick={this.execut}>
-            Update
-          </Button>
-        </Form.Item>
-        <Form.Item>
-          <Button type='primary' htmlType='submit'>
-            Delete
-          </Button>
-        </Form.Item>
-        <Table dataSource={data} columns={columns} />
-      </Form>
+      <div>
+        <Button onClick={this.handleAdd} type='primary' style={{ marginBottom: 16 }}>
+          Add a row
+        </Button>
+        <Table
+          components={components}
+          rowClassName={() => 'editable-row'}
+          bordered
+          dataSource={data}
+          columns={columns}
+        />
+      </div>
     );
   }
 }
 
-const WrappedDemo = Form.create({ name: 'customized_form_controls' })(Demo);
-
-storiesOf('Namespaces', module).add('prefix', () => <WrappedDemo />);
+storiesOf('Namespaces', module).add('prefix', () => <EditableTable />);
