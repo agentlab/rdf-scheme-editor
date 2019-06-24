53923d30eb6576253a06523082d7baa15bc98886
tatiana <tatiyana_sh@mail.ru>
2019-06-24 20:05:24 +0300

    Поправила поля ввода

--- src/components/Summary.stories.js
+++ src/components/Summary.stories.js
@@ -1,46 +1,14 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { Form, Input } from 'antd';
-import { any } from 'prop-types';
-
-// const server_URL = 'https://agentlab.ru/rdf4j-server';
 
 class FormLayoutDemo extends React.Component {
   constructor() {
     super();
-
     this.state = {
-      bindings: [
-        {
-          id: {
-            type: '',
-            value: '',
-          },
-          description: {
-            type: '',
-            value: '',
-          },
-          location: {
-            type: '',
-            value: '',
-          },
-          server: {
-            type: '',
-            value: '',
-          },
-          size: {
-            type: '',
-            value: '',
-          },
-          contexts: {
-            type: '',
-            value: '',
-          },
-        },
-      ],
+      formLayout: 'horizontal',
     };
   }
-
   handleExecute = async () => {
     const url = 'https://agentlab.ru/rdf4j-workbench/repositories/rpo-tests/summary';
     const res = await fetch(url, {
@@ -58,37 +26,38 @@ class FormLayoutDemo extends React.Component {
 
   render() {
     this.handleExecute();
+    const { formLayout } = this.state;
+    const formItemLayout =
+      formLayout === 'horizontal'
+        ? {
+            labelCol: { span: 4 },
+            wrapperCol: { span: 14 },
+          }
+        : null;
+
     return (
       <div>
-        <Form>
+        <Form layout={formLayout}>
           <h1 align='center'> Repository Location </h1>
 
-          <Form.Item label='ID:'>
-            <Input readOnly defaultValue='reqs' value={this.state.bindings[0].id.value} />
+          <Form.Item label='ID: ' {...formItemLayout}>
+            <Input readOnly defaultValue='reqs' />
           </Form.Item>
-          <Form.Item label='Title:'>
-            <Input readOnly defaultValue='Requirements' value={this.state.bindings[0].description.value} />
+          <Form.Item label='Title: ' {...formItemLayout}>
+            <Input readOnly defaultValue='Requirements' />
           </Form.Item>
-          <Form.Item label='Location:'>
-            <Input
-              readOnly
-              defaultValue='https://agentlab.ru/rdf4j-server/repositories/reqs'
-              value={this.state.bindings[0].location.value}
-            />
+          <Form.Item label='Location: ' {...formItemLayout}>
+            <Input readOnly defaultValue='https://agentlab.ru/rdf4j-server/repositories/reqs' />
           </Form.Item>
-          <Form.Item label='RDF4J Server:'>
-            <Input
-              readOnly
-              defaultValue='https://agentlab.ru/rdf4j-server'
-              value={this.state.bindings[0].server.value}
-            />
+          <Form.Item label='RDF4J Server: ' {...formItemLayout}>
+            <Input readOnly defaultValue='https://agentlab.ru/rdf4j-server' />
           </Form.Item>
           <h1 align='center'> Repository Size </h1>
-          <Form.Item label='Number of Statements:'>
-            <Input readOnly defaultValue='5672' value={this.state.bindings[0].size.value} />
+          <Form.Item label='Number of Statements: ' {...formItemLayout}>
+            <Input readOnly defaultValue='5672' />
           </Form.Item>
-          <Form.Item label='Number of Labeled Contexts: '>
-            <Input readOnly defaultValue='10' value={this.state.bindings[0].contexts.value} />
+          <Form.Item label='Number of Labeled Contexts: ' {...formItemLayout}>
+            <Input readOnly defaultValue='10' />
           </Form.Item>
         </Form>
       </div>
@@ -96,10 +65,4 @@ class FormLayoutDemo extends React.Component {
   }
 }
 
-//storiesOf('Repositories', module).add('List', () => <RepositoriesTable props={server_URL} />);
-/*
-          <form onSubmit={this.props.reqSend} style={{ marginTop: '10px' }}>
-            <button onClick={this.handleExecute}>Execute</button>
-          </form>
-*/
 storiesOf('Lab1', module).add('Summary', () => <FormLayoutDemo />);
