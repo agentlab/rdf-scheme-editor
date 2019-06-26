05fcb02962ad36ce1a5cea6818ccf984c30c07ff
akhabutdinova <akhabutdinova@gmail.com>
2019-06-27 00:35:38 +0300

    ?

---
+++ src/rdf4j-tests/sparql_post_without_context.test.js
@@ -0,0 +1,93 @@
+describe('test Post without context', function() {
+  var urlprefixReq = 'https://agentlab.ru/rdf4j-server/repositories/rpo-tests';
+  const data = '<http://exampleSub> <http://exampleSub> 87';
+  var params =
+    'action=exec&' +
+    'queryLn=SPARQL&' +
+    'query=' +
+    encodeURIComponent('PREFIX foaf: <' + urlprefixReq + '> ' + 'SELECT ?Subject ' + 'WHERE{ ' + data + '}') +
+    '&' +
+    'limit_query=100&' +
+    'infer=true&';
+
+  it('insert data to rdf repository without context', async () => {
+    var urlprefix = urlprefixReq + '/query' + '?' + params;
+    //проверка до добавления
+    var dataSel = await fetch(urlprefix, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/sparql-results+json',
+      },
+    }).then((r) => r.json());
+    expect(dataSel.results.bindings.length).toEqual(0);
+
+    //добавление
+    urlprefix = urlprefixReq + '/update';
+    var result = await fetch(urlprefix, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/sparql-query',
+      },
+      body: 'update=' + encodeURIComponent('INSERT DATA' + '{' + data + '}'),
+    }).then((r) => {
+      console.log(r);
+      expect(r).toBeInstanceOf(Response);
+      expect(r['status']).toEqual(200);
+      return r;
+    });
+  });
+
+  it('check data after insert to rdf repository without context', async () => {
+    //проверка после
+    var urlprefix = urlprefixReq + '/query' + '?' + params;
+    var dataSel = await fetch(urlprefix, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/sparql-results+json',
+      },
+    }).then((r) => r.json());
+    expect(dataSel.results.bindings.length).toBeGreaterThan(0);
+  });
+
+  it('delete data to rdf repository without context', async () => {
+    var urlprefix = urlprefixReq + '/query' + '?' + params;
+    //проверка до
+    var dataSel = await fetch(urlprefix, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/sparql-results+json',
+      },
+    }).then((r) => r.json());
+    expect(dataSel.results.bindings.length).toBeGreaterThan(0);
+
+    //удаление
+    urlprefix = urlprefixReq + '/update';
+    var result = await fetch(urlprefix, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/sparql-query',
+      },
+      body: 'update=' + encodeURIComponent('DELETE DATA' + '{' + data + '}'),
+    }).then((r) => {
+      console.log(r);
+      expect(r).toBeInstanceOf(Response);
+      expect(r['status']).toEqual(200);
+      return r;
+    });
+
+    //проверка после
+    dataSel = await fetch(urlprefix, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept: 'application/sparql-results+json',
+      },
+    }).then((r) => r.json());
+    expect(dataSel.results.bindings.length).toEqual(0);
+  });
+});
